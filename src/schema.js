5032f71e6871da92dd5a69cd6701bc61b145c7e9
RyRojas <30281811+RyRojas@users.noreply.github.com>
2022-01-13 13:40:09 -0800

    Defined schemas

---
+++ src/schema.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teach a specific topic"
+  type Track {
+    "Learning Track ID of type ID"
+    id: ID!
+    "Learning track Title"
+    title: String!
+    "Learning Track Author of type Author"
+    author: Author!
+    "URL for learning track thumbnail"
+    thumbnail: String
+    "Approximate time required (mins) to complete learning track"
+    length: Int
+    "Number of modules in learning track"
+    modulesCount: Int
+  }
+
+  "Author of a complete learning Track or Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
