91a4fa8c771c765854e618afcbba87b206a284a6
RyRojas <30281811+RyRojas@users.noreply.github.com>
2022-01-17 14:47:51 -0800

    Implemented track view incrementation datasource and resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -10,6 +10,29 @@ const resolvers = {
     },
   },
 
+  Mutation: {
+    //Increment Track's numberOfViews property
+    incrementTrackViews: async (_, { trackId }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(trackId);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${trackId}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
