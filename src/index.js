583a990db621d83def03511e2b9b46a00d185d1d
RyRojas <30281811+RyRojas@users.noreply.github.com>
2022-01-18 12:43:15 -0800

    Updated ApolloClient to use live server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://catstronauts-test-app.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
