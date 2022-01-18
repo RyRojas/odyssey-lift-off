5eb218bdc3bffd62fd899ebab5c5c8fc9c806ca3
RyRojas <30281811+RyRojas@users.noreply.github.com>
2022-01-18 12:21:04 -0800

    Configured Apollo Server for deployment

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    trackAPI: new TrackAPI(),
-  }),
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+      trackAPI: new TrackAPI(),
+    }),
+  });
 
-server.listen().then(() => {
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
     🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
   `);
-});
+}
+
+startApolloServer(typeDefs, resolvers);
